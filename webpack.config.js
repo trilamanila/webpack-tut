081481576c98c40302d85362e4c18dfd713938d2
JC <trilamanila@MacBook-Pro-8.local>
2019-10-19 06:28:52 -0400

    Added Sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,15 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   },
   module: {
-      rules: [
-          {
-              test: /\.css$/,
-              use: ["style-loader", "css-loader"]
-          }
-      ]
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader" //1. Turns sass into css
+        ]
+      }
+    ]
   }
 };
